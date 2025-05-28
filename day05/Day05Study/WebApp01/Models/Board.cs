1e33d90de1baeab1ec096abaaec351c59cd2a594
Naiery0 <molly724@naver.com>
2025-05-28 17:12:09 +0900

    5일차 커밋

---
+++ day05/Day05Study/WebApp01/Models/Board.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp01.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public string Description { get; set; }
+    }
+}
